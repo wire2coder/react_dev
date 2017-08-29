021922f9d9d59a338b73295ccf54708f6d18fdea
CNNENGRING for tape bar coders <cnnicentral@turner.com>
2017-08-29 02:37:08 -0400

    pass multiple 'props' object into component1 function

--- introapp/src/index.js
+++ introapp/src/index.js
@@ -13,9 +13,19 @@ var ReactDom = require('react-dom');
 
 // make a component out of function
 // CAPITALISE your component name to tell React that it is not a 'native' component
-var Component1 = function() {
+
+var Component1 = function(props) {
     // a function that returns JSX, Javascript Syntax Extension
-    return ( <h4>Let us make 92000 </h4> );
+
+    var style = {
+        color: props.color
+    }
+
+    return ( 
+        <div style={style} >
+        <h4>{props.greeting}</h4>  
+        </div>
+    );
 }
 
 // render the component we just created
@@ -25,4 +35,13 @@ var Component1 = function() {
 // ReactDom.render(component1, document.getElementById('root'));
 // ReactDom.render(React.createElement(Component1), document.getElementById('root') );
 // ReactDom.render(<Component1></Component1>, document.getElementById('root'));
-ReactDom.render(<Component1 />, document.getElementById('root') );
+
+// Now are we adding properties to the Component/function
+ReactDom.render(
+    // greeting='hello' is attached to object 'props'
+    <div>
+    <Component1 greeting='Let us make a lot of money' color='red' />
+    <Component1 greeting='Let us be bold and kind' color='blue' />
+    </div>,
+    document.getElementById('root') 
+);
