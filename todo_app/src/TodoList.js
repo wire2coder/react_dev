ac8f9a31d12a19b2fe6bfd97898ae501e48c699b
Andy Murph <murph@email.com>
2017-09-18 06:09:01 -0400

    refactored <TodoList> with 'map' method

--- todo_app/src/TodoList.js
+++ todo_app/src/TodoList.js
@@ -2,21 +2,37 @@ var React = require('react');
 
 var TodoItem = require('./TodoItem.js');
 
-var TodoList = function(props) {
 
+var TodoList = function(props) {
+    var todos = props.todos;
     var todoList = [];
 
-    for(var i=0; i < props.todos.length; i++) {
-        todoList.push( 
+    // for(var i=0; i < props.todos.length; i++) {
+    //     todoList.push( 
+    //         <TodoItem 
+    //             todoItem= {props.todos[i]} 
+    //             key = {props.todos[i].id}
+
+    //             onDeleteButtonClick={props.handleDeleteButtonClick} 
+    //             onCheckboxClick = {props.onCheckboxClick}
+    //         /> 
+    //     )
+    // }
+
+    // refactoring the code to be 'declaretive' with 'MAP' method
+    // instead of using for-loop
+    todoList = todos.map(function(todo) {
+        return (
             <TodoItem 
-                todoItem= {props.todos[i]} 
-                key = {props.todos[i].id}
+                todoItem = {todo}
+                key = {todo.id}
 
                 onDeleteButtonClick={props.handleDeleteButtonClick} 
                 onCheckboxClick = {props.onCheckboxClick}
-            /> 
-        )
-    }
+            />
+        );
+        
+    })
 
     // return JSX
     return (
